70e12a5d39038a65c172999e7a8096f39fc9434e
amazingTest <523314409@qq.com>
2019-08-22 17:26:06 +0800

    [fix](backend/utils/common) 修复字典中存在json字符串时的转化问题

--- backend/utils/common.py
+++ backend/utils/common.py
@@ -452,7 +452,7 @@ def validate_and_pre_process_import_test_case(case_suite_model, testing_case_mod
                         list(map(lambda x: ast.literal_eval(x.replace('\'', '\"')),
                              str(_case_info[key]).strip().split(';')))
             elif attribute_type is dict:
-                _case_info[key] = ast.literal_eval(str(_case_info[key]).strip().replace('\'', '\"')) \
+                _case_info[key] = ast.literal_eval(str(_case_info[key]).strip()) \
                     if _case_info[key] else {}
             elif attribute_type is bool:
                 _case_info[key] = True if str(_case_info[key]).strip().lower() == 'true' else False
