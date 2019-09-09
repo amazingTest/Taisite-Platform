d6cbbc13a7ec2f4bc45d72832f8550095a2a52fb
shaoyuyishiwo <523314409@qq.com>
2019-09-09 19:21:59 +0800

    [fix](Globalhost.vue)修复IP端口仅能输入4位数的问题

--- frontend/src/views/interfaceTestProject/global/Globalhost.vue
+++ frontend/src/views/interfaceTestProject/global/Globalhost.vue
@@ -196,7 +196,7 @@
                                 + '([0-9a-z_!~*\'()-]+.)*' // 域名- www.
                                 + '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' // 二级域名
                                 + '[a-z]{2,6})' // first level domain- .com or .museum
-                                + '(:[0-9]{1,4})?' // 端口- :80
+                                + '(:[0-9]{1,5})?' // 端口- :80
                                 + '((/?)|' // a slash isn't required if there is no file name
                                 + '(/[0-9a-z_!~*\'().;?:@&=+$,%#-]+)+/?)$';
                 let re = new RegExp(strRegex);
