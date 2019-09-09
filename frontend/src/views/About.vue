d4592e164952f0fe9e8a6a5e6de14e50cd58370e
shaoyuyishiwo <523314409@qq.com>
2019-09-10 00:06:09 +0800

    [feat](About.vue)更新关于作者页面

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -17,6 +17,19 @@
     </br>
     </br>
     <div style="font-size:20px; text-align: center;">
+      若您想在自动化测试上更近一步，欢迎扫描下方海报二维码、泰斯特带你成长~
+    </div>
+    </br>
+    </br>
+    </br>
+    <div align="center">
+      <img src="../assets/imgs/planetPoster.jpg">
+      </img>
+    </div>
+    </br>
+    </br>
+    </br>
+    <div style="font-size:20px; text-align: center;">
       若您对「泰斯特平台」有任何吐槽或想对平台有更深入的了解，欢迎扫描下方二维码关注我并了解最新资讯。
     </div>
     </br>
