6329b1ebcfdd45538c6a47c7dc120c537368885d
amazingTest <523314409@qq.com>
2020-11-08 11:56:18 +0800

    [feat]

--- backend/models/testReport.py
+++ backend/models/testReport.py
@@ -4,6 +4,7 @@
 from app import db
 from utils.mango import *
 from utils import common
+from utils.helpers import ExcelHelper
 import xlsxwriter
 from io import BytesIO
 import ast
@@ -31,6 +32,7 @@ test_report_detail_map = {
     "['testBaseInfo', 'headers']": '请求头',
     "['testBaseInfo', 'cookies']": '请求Cookie',
     "['testBaseInfo', 'presendParams']": '请求参数',
+    "['testBaseInfo', 'curl']": '复现 curl',
     "['testBaseInfo', 'checkHttpCode']": '状态码校验',
     "['responseHttpStatusCode']": '实际状态码',
     "['testBaseInfo', 'checkResponseData']": '数据校验',
@@ -80,9 +82,21 @@ class TestReport(Model):
         summary_sheet = workbook.add_worksheet(u'测试报告概览')
         detail_sheet = workbook.add_worksheet(u'测试报告详情')
 
+        # 设置测试报告表头 format
+
+        header_style = workbook.add_format()
+        header_style.set_bg_color("#00CCFF")
+        header_style.set_color("#FFFFFF")
+        header_style.set_bold()
+        header_style.set_border()
+
         # 测试报告概览表头
         for index, value in enumerate(test_report_summary_map.values()):
-            summary_sheet.write(0, index, value)
+            summary_sheet.write(0, index, value, header_style)
+
+        # 设置测试报告概览每列宽度
+        [ExcelHelper.ExcelSheetHelperFunctions.set_column_auto_width(summary_sheet, i)
+         for i in range(len(test_report_summary_map.values()))]
 
         # 测试报告概览数据
         for index, value in enumerate(test_report_summary_map.keys()):
@@ -92,13 +106,51 @@ class TestReport(Model):
 
         # 测试报告详情表头
         for index, value in enumerate(test_report_detail_map.values()):
-            detail_sheet.write(0, index, value)
+            detail_sheet.write(0, index, value, header_style)
+
+        # 设置测试报告详情每列宽度
+        [ExcelHelper.ExcelSheetHelperFunctions.set_column_auto_width(detail_sheet, i)
+         for i in range(len(test_report_detail_map.values()))]
+
+        test_result_pass_style = workbook.add_format()
+        test_result_pass_style.set_bg_color("#00ff44")
+        test_result_pass_style.set_color("#FFFFFF")
+        test_result_pass_style.set_bold()
+        # test_result_pass_style.set_border()
+
+        test_result_failed_style = workbook.add_format()
+        test_result_failed_style.set_bg_color("#ff0026")
+        test_result_failed_style.set_color("#FFFFFF")
+        test_result_failed_style.set_bold()
+        # test_result_failed_style.set_border()
+
+        failed_curl_style = workbook.add_format()
+        failed_curl_style.set_bg_color("#ffee00")
+        # failed_curl_style.set_color("#FFFFFF")
+        failed_curl_style.set_bold()
+        # failed_curl_style.set_border()
 
         # 测试报告详情数据
         for index, locator in enumerate(test_report_detail_map.keys()):
             locator = ast.literal_eval(locator)
             for col_index, detail in enumerate(test_details):
-                detail_sheet.write(col_index + 1, index, str(common.dict_get(detail, locator)))
+                if 'testConclusion' in str(locator):
+                    test_result = str(common.dict_get(detail, locator))
+                    if '测试通过' in test_result:
+                        detail_sheet.write(col_index + 1, index,
+                                           test_result, test_result_pass_style)
+                    else:
+                        detail_sheet.write(col_index + 1, index,
+                                           test_result, test_result_failed_style)
+                elif 'curl' in str(locator):
+                    test_result_status = str(common.dict_get(detail, ['status']))
+                    if test_result_status == 'failed':
+                        detail_sheet.write(col_index + 1, index,
+                                           str(common.dict_get(detail, locator)), failed_curl_style)
+                    else:
+                        detail_sheet.write(col_index + 1, index, str(common.dict_get(detail, locator)))
+                else:
+                    detail_sheet.write(col_index + 1, index, str(common.dict_get(detail, locator)))
 
         workbook.close()
 
